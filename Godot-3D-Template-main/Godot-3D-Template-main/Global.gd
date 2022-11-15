92d56d01ce053f79e08edf773582a61ba8ad9492
DanielGramling9 <112036402+DanielGramling9@users.noreply.github.com>
2022-11-14 20:49:54 -0500

    Completed

--- Godot-3D-Template-main/Godot-3D-Template-main/Global.gd
+++ Godot-3D-Template-main/Godot-3D-Template-main/Global.gd
@@ -1,5 +1,8 @@
 extends Node
 var menu = null
+const SAVE_PATH = "res://settings.cfg"
+var save_file = ConfigFile.new()
+var inputs = ["left","right","forward","back"]
 func _ready():
 	pause_mode = Node.PAUSE_MODE_PROCESS
 
@@ -14,3 +17,24 @@ func _unhandled_input(_event):
 			else:
 				get_tree().paused = false
 				menu.hide()
+				
+				
+
+func load_input():
+	var error = save_file.load(SAVE_PATH)
+	if error != OK:
+		print("Failed loading file")
+		return
+	
+	for i in inputs:
+		var key = save_file.get_value("Inputs", i, null)
+		InputMap.action_erase_events(i)
+		InputMap.action_add_event(i, key)
+
+func save_input():
+	for i in inputs:
+		var actions = InputMap.get_action_list(i)
+		for a in actions:
+			save_file.set_value("Inputs", i, a)
+	save_file.save(SAVE_PATH)
+
